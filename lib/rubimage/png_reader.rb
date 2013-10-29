ead38c4b9e0713e27935ad04ec4ac67a860f39df
Jamison Greeley <berwyn.codeweaver@gmail.com>
2013-10-29 17:39:04 -0600

    PngReader#dimensions now returns a hash

--- lib/rubimage/png_reader.rb
+++ lib/rubimage/png_reader.rb
@@ -13,7 +13,7 @@ module Rubimage
         skip_bytes PNG_HEADER
         @width  = next_int
         @height = next_int
-        @dimensions = [@width, @height]
+        @dimensions = {height: @height, width: @width}
         close
       end
   end
